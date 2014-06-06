0184e45c339d54bcb295f05d1c5a21915bc0ec0a
François Berder <fberder@hotmail.fr>
2014-06-06 20:52:25 +0100

    finished creating logger

---
+++ src/utils/Logger.cpp
@@ -0,0 +1,28 @@
+#include "Logger.hpp"
+
+Logger::Logger():
+m_enabled(true),
+m_file()
+{
+}
+
+Logger& Logger::instance()
+{
+    static Logger l;
+    return l;
+}
+
+void Logger::writeToFile(const std::string& fileName)
+{
+    m_file.open(fileName.c_str());
+    if(!m_file)
+    {
+        std::cout << "Could not log to " << fileName << "." << std::endl;
+        std::cout << "Fallback to console.";
+    }
+}
+
+void Logger::writeToConsole()
+{
+    m_file.close();
+}
