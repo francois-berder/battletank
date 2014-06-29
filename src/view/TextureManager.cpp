c673b85d36df0c69ebc5d66f53a644751c550961
François Berder <fberder@hotmail.fr>
2014-06-29 20:06:36 +0100

    Implemented texture manager

---
+++ src/view/TextureManager.cpp
@@ -0,0 +1,41 @@
+#include <stdexcept>
+#include <sstream>
+
+#include "TextureManager.hpp"
+
+TextureManager::TextureManager():
+m_textures()
+{
+}
+
+TextureManager& TextureManager::instance()
+{
+    static TextureManager tm;
+    return tm;
+}
+
+TexturePtr TextureManager::get(const std::string& name)
+{
+    auto it = m_textures.find(name);
+    if(it != m_textures.end())
+        return it->second;
+
+    std::string path = "media/textures/" + name;
+    m_textures[name] = load(path);
+
+    return m_textures[name];
+}
+
+TexturePtr TextureManager::load(const std::string &path)
+{
+    sf::Texture *tex = new sf::Texture;
+    if(!tex->loadFromFile(path))
+    {
+        delete tex;
+        std::stringstream msg;
+        msg << "Could not load texture from file " << path;
+        throw std::runtime_error(msg.str());
+    }
+    
+    return TexturePtr(tex);
+}
---
+++ src/view/TextureManager.hpp
@@ -0,0 +1,34 @@
+#ifndef __TEXTURE_MANAGER_H__
+#define __TEXTURE_MANAGER_H__
+
+#include <map>
+#include <string>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+
+typedef std::shared_ptr<sf::Texture> TexturePtr;
+
+class TextureManager
+{
+    public :
+    
+
+        TextureManager(const TextureManager& tm) = delete;
+        TextureManager& operator=(const TextureManager& tm) = delete;
+        
+        static TextureManager& instance();
+        
+        TexturePtr get(const std::string& name);
+        
+    private :
+
+        TextureManager();
+
+        TexturePtr load(const std::string &path);
+        
+        std::map<std::string, TexturePtr> m_textures;
+};
+
+#endif /* __TEXTURE_MANAGER_H__ */
+
