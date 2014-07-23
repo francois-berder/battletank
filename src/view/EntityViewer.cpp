ac54fb74c2b9deac05ad17777520bd98c7328a4f
Francois Berder <fberder@hotmail.fr>
2014-07-23 20:20:27 +0100

    Implemented function EntityViewer::drawAll()

--- src/view/EntityViewer.cpp
+++ src/view/EntityViewer.cpp
@@ -1,5 +1,6 @@
 #include "EntityViewer.hpp"
 #include "TextureManager.hpp"
+#include "Utils.hpp"
 
 
 EntityViewer::EntityViewer(sf::RenderWindow &renderWindow, Array &entities):
@@ -10,7 +11,28 @@ m_entities(entities)
 
 void EntityViewer::drawAll()
 {
-    // TODO: Implement function
+    for(unsigned int i = 0; i < m_entities.size(); ++i)
+    {
+        Map &entity = m_entities[i]->asMap();
+        std::string type = entity["type"]->asData().getValue();
+        
+        if(type == "obstacle")
+        {
+            Map &pos = entity["pos"]->asMap();
+            float x = toFloat(pos["x"]->asData().getValue());
+            float y = toFloat(pos["y"]->asData().getValue());
+            drawObstacle(x, y);
+        }
+        else if(type == "tank")
+        {
+            Map &pos = entity["pos"]->asMap();
+            float x = toFloat(pos["x"]->asData().getValue());
+            float y = toFloat(pos["y"]->asData().getValue());
+            float angle = toFloat(entity["angle"]->asData().getValue());
+            unsigned int health = toUInteger(entity["health"]->asData().getValue());
+            drawTank(x, y, angle, health);
+        }
+    }
 }
 
 void EntityViewer::drawObstacle(float x, float y)
