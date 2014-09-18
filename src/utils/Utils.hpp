429c81d62e03a64fe134008c94bc5c7d48052954
Francois Berder <fberder@hotmail.fr>
2014-09-18 18:33:07 +0200

    utils: toString function

--- src/utils/Utils.hpp
+++ src/utils/Utils.hpp
@@ -1,6 +1,7 @@
 #ifndef __UTILS_HPP__
 #define __UTILS_HPP__
 
+#include <sstream>
 #include <string>
 #include <list>
 
@@ -13,6 +14,14 @@ bool isAlphaNumeric(const char c);
 unsigned int toUInteger(const std::string &str);
 int toInteger(const std::string &str);
 float toFloat(const std::string &str);
+
+template<typename T>
+std::string toString(T t)
+{
+    std::stringstream ss;
+    ss << t;
+    return ss.str();
+}
 
 std::list<std::string> split(const std::string &str, const char sep);
 std::string concat(std::list<std::string> &elems, const char sep);
